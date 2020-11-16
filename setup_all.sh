1d08b2f3d64458b8aca856dc1ff5c11fdd838f7e
LLlKuIIeP <79032312801@yandex.ru>
2020-11-17 00:43:45 +0300

    удалил не нужные файлы

--- setup_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-echo -e "\n\n\tInstall Boost\n"
-ansible-playbook -i inventory/dev playbooks/install_boost.yml
-
-
-echo -e "\n\n\tInstall VTK\n"
-ansible-playbook -i inventory/dev playbooks/install_vtk.yml
